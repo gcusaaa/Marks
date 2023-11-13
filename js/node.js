4324c5b9bf52c3f26d13b32b77a0f0a0241f1423
gcusaaa <gcusaaa@gmail.com>
2023-11-13 16:43:16 +0200

    marks

---
+++ js/node.js
@@ -0,0 +1,27 @@
+let marks = 0
+switch (marks) {
+    case 100:
+        marks = A+
+        break;
+    case 90-99:
+        marks = A
+        break;
+    case 80-89:
+        marks = B+
+        break;
+    case 70-79:
+        console.log("Distinction")
+        break;
+    case 60-69:
+        console.log("C+")
+        break;
+    case 50-59:
+        console.log("Passed")
+        break;
+    case 0 >= 49:
+        console.log("Passed")
+        break;
+
+    default:
+        break;
+}
